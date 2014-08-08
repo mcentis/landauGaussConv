094661a637afb0b3d4f0d65d4ddd14c4d3dcfc74
mcentis <matteo.centis.vignali@desy.de>
2014-08-08 19:17:08 +0200

    added graphs of lanMPV and Gsig of the fit the fitted sigma is always very small, to be investigated!

--- src/langausPlay.cpp
+++ src/langausPlay.cpp
@@ -1,6 +1,11 @@
 #include "TSystem.h"
 #include "TFile.h"
-#include "TH1F.h"
+#include "TH1.h"
+#include "TF1.h"
+#include "TDirectory.h"
+#include "TGraphErrors.h"
+#include "TCanvas.h"
+#include "TAxis.h"
 
 #include "iostream"
 
@@ -22,6 +27,11 @@ int main(int argc, char* argv[])
   double lanMPV;
   double lanWidth;
   double gausSig;
+  TH1* hist;
+  TF1* fit;
+  char name[200];
+  char title[400];
+  int nPoint;
 
   // big axis labels
   gStyle->SetLabelSize(0.05, "x");
@@ -37,18 +47,54 @@ int main(int argc, char* argv[])
   gStyle->SetLineWidth(1);
 
   TFile* outFile = new TFile(argv[1], "RECREATE");
-  outFile->cd();
-  TH1* hist;
+  TDirectory* histDir = outFile->mkdir("histograms");
+  histDir->cd();
+
+  TCanvas* servCan = new TCanvas(); // used for fitting and drawing stuff
 
   lanMPV = 40;
   lanWidth = 4;
+
+  sprintf(name, "fitLanMPV_genLanMPV_%.00f_genLanWidth_%.00f", lanMPV, lanWidth);
+  sprintf(title, "Fitted Landau MPV vs generator gaus sigma, generator: lanMPV %.00f, lanWidth %.00f", lanMPV, lanWidth);
+  TGraphErrors* fitMPVvsGenSig = new TGraphErrors();
+  fitMPVvsGenSig->SetName(name);
+  fitMPVvsGenSig->SetTitle(title);
+
+  sprintf(name, "fitGausSig_genLanMPV_%.00f_genLanWidth_%.00f", lanMPV, lanWidth);
+  sprintf(title, "Fitted Gaus sigma vs generator gaus sigma, generator: lanMPV %.00f, lanWidth %.00f", lanMPV, lanWidth);
+  TGraphErrors* fitGsigvsGenSig = new TGraphErrors();
+  fitGsigvsGenSig->SetName(name);
+  fitGsigvsGenSig->SetTitle(title);
 
   for(int i = 0; i < 10; ++i)
     {
       gausSig = i * 2;
       hist = genLangaus(nEntries, lanMPV, lanWidth, gausSig);
+      fit = lanGausFit(hist, 10, 100); // warning: fixed parameters!!!!
       hist->Write();
+
+      nPoint = fitMPVvsGenSig->GetN();
+      fitMPVvsGenSig->SetPoint(nPoint, gausSig, fit->GetParameter(1));
+      fitMPVvsGenSig->SetPointError(nPoint, 0, fit->GetParError(1));
+
+      fitGsigvsGenSig->SetPoint(nPoint, gausSig, fit->GetParameter(3));
+      fitGsigvsGenSig->SetPointError(nPoint, 0, fit->GetParError(3));
     }
+
+  fitMPVvsGenSig->Draw("AP");
+  fitMPVvsGenSig->GetXaxis()->SetTitle("Generator Gauss sigma [A. U.]");
+  fitMPVvsGenSig->GetYaxis()->SetTitle("Fitted Landau MPV [A. U.]");
+
+  fitGsigvsGenSig->Draw("AP");
+  fitGsigvsGenSig->GetXaxis()->SetTitle("Generator Gauss sigma [A. U.]");
+  fitGsigvsGenSig->GetYaxis()->SetTitle("Fitted Gauss sigma [A. U.]");
+
+  delete servCan;
+
+  outFile->cd();
+  fitMPVvsGenSig->Write();
+  fitGsigvsGenSig->Write();
 
   outFile->Close();
 
